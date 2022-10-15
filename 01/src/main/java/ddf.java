6a5e0fe10fe65197dc5dfa4e0a140ca62c5f3d80
EasyMoneyXj <xj0329zz@163.com>
2022-10-15 17:34:00 +0800

    third commit

--- 01/src/main/java/ddf.java
+++ 01/src/main/java/ddf.java
@@ -2,5 +2,6 @@ public class ddf {
     public static void main(String[] args) {
         System.out.println("jdisdjisd");
         System.out.println("hudfhiosdhosfj");
+        System.out.println("uishduihio");
     }
 }
